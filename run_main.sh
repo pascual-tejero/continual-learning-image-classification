62ad55d82160b96b50c0df4d78e5cd06b293e342
Pascual <116434029+pascutc98@users.noreply.github.com>
2024-01-11 17:54:10 +0100

    Update run_main.sh

--- run_main.sh
+++ run_main.sh
@@ -1,4 +1,4 @@
-python main.py --exp_name "mnist_training2" --dataset mnist --memory_size 59900
+python main.py --exp_name "mnist_training2" --dataset mnist --memory_size 29950
 python main.py --exp_name "cifar10_training2" --dataset cifar10 --memory_size 24525
 python main.py --exp_name "cifar100_training2" --dataset cifar100 --memory_size 22531
 python main.py --exp_name "cifar100_alternative_dist" --dataset cifar100_alternative_dist --memory_size 36506
