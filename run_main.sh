2f59d808acaeb2ef480324ec6c5672095a983741
pascutc98 <pascu1998@hotmail.com>
2024-01-15 17:26:10 +0100

    Added code

--- run_main.sh
+++ run_main.sh
@@ -1,5 +1,11 @@
 
 # If number of tasks is 2:
+
+# CIFAR100 with alternative distribution: 36490 samples in task 1 and 8945 samples in task 2 (with leakage data)
+python main.py --exp_name "cifar100_leakage_data_mem50%" --dataset cifar100_alternative_dist --memory_size 22500 # 22500/100 = 225 samples per class
+python main.py --exp_name "cifar100_leakage_data_mem30%" --dataset cifar100_alternative_dist --memory_size 13500 # 13500/100 = 135 samples per class
+python main.py --exp_name "cifar100_leakage_data_mem10%" --dataset cifar100_alternative_dist --memory_size 4500 # 4500/100 = 45 samples per class
+
 # MNIST and Fashion-MNIST: 59900 samples per task 
 python main.py --exp_name "mnist_mem50%" --dataset mnist --memory_size 29745 # 29745/10 = 2974 samples per class
 python main.py --exp_name "mnist_mem30%" --dataset mnist --memory_size 17847 # 17847/10 = 1784 samples per class
@@ -15,7 +21,3 @@ python main.py --exp_name "cifar100_mem50%" --dataset cifar100 --memory_size 225
 python main.py --exp_name "cifar100_mem30%" --dataset cifar100 --memory_size 13500 # 13500/100 = 135 samples per class
 python main.py --exp_name "cifar100_mem10%" --dataset cifar100 --memory_size 4500 # 4500/100 = 45 samples per class
 
-# CIFAR100 with alternative distribution: 36490 samples in task 1 and 8945 samples in task 2 (with leakage data)
-python main.py --exp_name "cifar100_leakage_data_mem50%" --dataset cifar100_alternative_dist --memory_size 22500 # 22500/100 = 225 samples per class
-python main.py --exp_name "cifar100_leakage_data_mem30%" --dataset cifar100_alternative_dist --memory_size 13500 # 13500/100 = 135 samples per class
-python main.py --exp_name "cifar100_leakage_data_mem10%" --dataset cifar100_alternative_dist --memory_size 4500 # 4500/100 = 45 samples per class
