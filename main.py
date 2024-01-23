9a0fe2aaf46bc74411c85c7823d768813be17596
Pascual <pascu1998@hotmail.com>
2024-01-23 14:52:56 +0100

    Added code

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
     # General parameters
     argparse.add_argument('--exp_name', type=str, default="CL_methods")
     argparse.add_argument('--seed', type=int, default=0)
-    argparse.add_argument('--epochs', type=int, default=1) # 500
+    argparse.add_argument('--epochs', type=int, default=500) # 500
     argparse.add_argument('--lr', type=float, default=0.001) # 0.001
     argparse.add_argument('--lr_decay', type=float, default=5) # 5
     argparse.add_argument('--lr_patience', type=int, default=10) # 10
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     argparse.add_argument('--num_tasks', type=int, default=2) # 2
 
     # Dataset parameters: mnist, cifar10, cifar100, cifar100-alternative-dist
-    argparse.add_argument('--dataset', type=str, default="cifar100")
+    argparse.add_argument('--dataset', type=str, default="cifar100-alternative-dist")
 
     # EWC parameters
     argparse.add_argument('--ewc_lambda' , type=float, default=1000) # 1000
